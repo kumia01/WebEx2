baeb3291a18492bee4892fd2555a8fecfe78b9e6
StormVMSC <s351936@oslomet.no>
2022-11-09 16:19:24 +0100

    Endringer på front-end

--- WebEx2/ClientApp/src/components/Home.js
+++ WebEx2/ClientApp/src/components/Home.js
@@ -1,33 +1,27 @@
 import React, { Component } from 'react';
+import { Button, Form, Container, Col, Row, Card, CardTitle, CardText } from 'reactstrap';
 
 export class Home extends Component {
     static displayName = Home.name;
 
     render () {
         return (
-            /*<div className="container" id="mainbox">
-                <div className="row d-flex justify-content-center">
-                    <div className="col-md-6" id="tekstkolonne">
-                        <div className="panel-heading fw-bold fs-4" id="overskrift">
-                            Hjelper unge voksne <br />
-                            <span style={{ color: "red" }}>vinne økonomisk</span>
-                        </div>
+            <Container id="mainbox">
+                <Row>
+                    <Col sm="6">
+                        <Card>
+                            <CardTitle>
+                                <p className="h5 font-weight-bold">Hjelper unge voksne vinne økonomisk</p>
+                            </CardTitle>
 
-                        <div className="panel-body" id="maintekst">
-                            <p>
-                                Ghost Finance er en brukervennlig aksjehandel- og investeringsplattform for unge investorer som deg.
-                            </p>
-                            <p>
-                                Jevnlig investering kan hjelpe deg bygge kapital og oppnå dine økonomiske mål.
-                            </p>
-                        </div>
-                    </div>
-                    <div className="col-md-6" id="bildekolonne">
-
-                    </div>
-                </div>
-            </div>*/
-            <p>hei</p>
+                            <CardText>
+                                Ghost Finance er en brukervennlig aksjehandel- og investeringsplattform for unge investorer som deg.<br />
+                                Jevnlig investering kan hjelpe deg bygge kapital og oppnå dine økonomiske mål. 
+                            </CardText>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 }
