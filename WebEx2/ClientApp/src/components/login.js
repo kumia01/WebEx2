8072707fe88e0a4acebccc5df142b18ae42393c1
StormVMSC <s351936@oslomet.no>
2022-11-09 13:59:23 +0100

    Login + litt css

---
+++ WebEx2/ClientApp/src/components/login.js
@@ -0,0 +1,40 @@
+﻿import React, { Component } from 'react';
+
+export class Login extends Component {
+    static displayName = Login.name;
+
+    render() {
+        return (
+            <div class="container">
+                <form class="form-registrer">
+                    <div class="col-md-4">
+                        <h2>Log in</h2>
+
+                        <div class="form-group">
+                            <label for="fornavn">Brukernavn</label>
+                            <input type="text" placeholder="Brukernavn" class="form-control" id="brukernavn" required="required" />
+                        </div>
+
+                        <div class="form-group">
+                            <label for="etternavn">Passord</label>
+                            <input type="text" placeholder="Passord" class="form-control" id="passord" required="required" />
+                        </div>
+
+                        <div class="form-group">
+                            <div class="btnLogIn">
+                                <button class="btn btn-primary">Logg Inn</button>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="linkRegBruker">
+                                <a class="link-primary">Opprett Bruker</a>
+                            </div>
+                        </div>
+
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
