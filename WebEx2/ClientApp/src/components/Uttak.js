daa8e68aa7e0b8ba5ca870fc8a9716adcd3c942e
StormVMSC <s351936@oslomet.no>
2022-11-15 14:53:47 +0100

    Endringer til css og index.html

--- WebEx2/ClientApp/src/components/Uttak.js
+++ WebEx2/ClientApp/src/components/Uttak.js
@@ -9,24 +9,37 @@ export class Uttak extends Component {
         return (
             <Container>
                 <Row fluid className="align-items-center">
-                    <Col fluid>
-                        <h3 style={{ color: "#023e73" }}>Overføringer</h3>
-                        <Button className="btn btn-primary btn-sm">Overfør</Button>  
-                        <Button className="btn btn-primary btn-sm">Transaksjoner</Button>
+                    <Col fluid sm="3">
+                        <h3 style={{ color: "#023e73" }} className="text-center">Overføringer</h3>
+                        <Row fluid>
+                            <Col fluid className="text-center">
+                            </Col>
+                        </Row>
+                    </Col>
+
+                    <Col sm="3">
+                    </Col>
+
+                    <Col fluid sm="6" className="text-center">
+                        <h3 style={{ color: "#023e73"  }}>Hvordan ønsker du å sette inn penger?</h3>
                     </Col>
                 </Row>
 
                 <Row fluid className="align-items-center">
-                    <Col fluid className="btn-group-vertical mt-3 costum-btn" sm="">
-                        <Button className="btn btn-primary btn-sm mb-1 ">Overfør</Button>
+                    <Col fluid className="btn-group-vertical mt-3" sm="3">
+                        <Button className="btn btn-primary btn-sm mb-1">Overfør</Button>
                         <Button className="btn btn-primary btn-sm mb-1">Uttak</Button>
                         <Button className="btn btn-primary btn-sm mb-1">Aktiva</Button>
                         <Button className="btn btn-primary btn-sm mb-1">Overfør aksjer</Button>
                     </Col>
 
-                    <Col fluid sm="9" className="justify-content-center">
-                        <h3 style={{ color: "#023e73" }}>Overføringer</h3>
-                        <h4 style={{ color: "#023e73" }}>Hvordan ønsker du å sette inn penger?</h4>
+                    <Col sm="3"></Col>
+
+                    <Col fluid className="btn-group-vertical mt-3" sm="6">
+                        <Button className="btn btn-md mb-2" color="primary">Vipps</Button>{' '}
+                        <Button className="btn btn-md mb-2" color="primary">Apple Pay</Button>{' '}
+                        <Button className="btn btn-md mb-2" color="primary">Credit- eller debitkort</Button>{' '}
+                        <Button className="btn btn-md mb-2" color="primary">Bankoverføring</Button>{' '}
                     </Col>
                 </Row>
             </Container>
